875bdc0226ab5939d2f855a73ce533143d45f46c
k0r1 <minhquan.k0r1@gmail.com>
2022-04-08 00:51:38 +0700

    Update

--- cmd/block/main.go
+++ cmd/block/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/quankori/go-eth/internal/file"
 	"github.com/quankori/go-eth/pkg/block"
 )
 
 func main() {
-	data := file.ReadFile()
+	// data := file.ReadFile()
 	blockNumber := make(chan uint64)
 	go block.SubscribeNewBlock(blockNumber)
 	blockResult := <-blockNumber
 	fmt.Println(blockResult)
-	blockResultData := block.GetBlockHash(data)
-	for _, tx := range blockResultData.Transactions() {
-		fmt.Println(tx.Hash().Hex())
+	for msg := range blockNumber { //will break when ch closes
+		fmt.Println(msg)
 	}
+	// blockResultData := block.GetBlockHash(data)
+	// for _, tx := range blockResultData.Transactions() {
+	// 	fmt.Println(tx.Hash().Hex())
+	// }
 }
